3a82a2cf4bb3aca22b1b1852e99585dac2beb7ca
Dejan Jurisic <dejanjurisic@yahoo.com>
2017-08-09 16:08:20 +0200

    etnokeramikaWebShop

--- etnokeramikaWebShop/eshop.Master.designer.cs
+++ etnokeramikaWebShop/eshop.Master.designer.cs
@@ -49,15 +49,6 @@ namespace etnokeramikaWebShop {
         protected global::System.Web.UI.WebControls.LinkButton btnSearch;
         
         /// <summary>
-        /// mainMenuVertical1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::etnokeramikaWebShop.userControls.MainMenuVertical mainMenuVertical1;
-        
-        /// <summary>
         /// masterMenu1 control.
         /// </summary>
         /// <remarks>
