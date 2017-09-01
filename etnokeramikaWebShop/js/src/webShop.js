c9f0f0246d93b0d6175aff5fa99b75f075bd16be
Dejan Jurisic <dejanjurisic@yahoo.com>
2017-09-01 02:11:14 +0200

    etnokeramikaWebShop

--- etnokeramikaWebShop/js/src/webShop.js
+++ etnokeramikaWebShop/js/src/webShop.js
@@ -3,6 +3,8 @@
     var right = $(window).width() - ($('#btnCartFp').offset().left + $('#btnCartFp').width());
     $('#cartFpContainer').css({ top: top, right: right });
     $('#cartFpContainer').show();
+
+    GetCartItems();
 }
 
 function AddToCart(lblProductID) {
@@ -24,10 +26,36 @@ function AddToCart(lblProductID) {
     
 }
 
+function GetCartItems() {
+    $.ajax({
+        type: 'POST',
+        url: 'WebMethods.aspx/GetCart',
+        data: '',
+        contentType: 'application/json;charset=utf-8',
+        dataType: 'json',
+        success: function (response) {
+            $('#tblCartItems tr').remove();
+            $.get('/jQueryTemplates/cartFPTemplate.html', null, function (cartTemplate) {
+                $.tmpl(cartTemplate, JSON.parse(response.d)).appendTo('#tblCartItems');
+            })
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert(jqXHR.responseText);
+        }
+    })
+}
+
+//document
 $(window).scroll(function () {
     if($('#cartFpContainer').is(':visible')){
         var scrollTop = $(window).scrollTop();
         var btnCartFpTop = $('#btnCartFp').offset().top + $('#btnCartFp').height();
         $('#cartFpContainer').css({ top: scrollTop > btnCartFpTop ? scrollTop : btnCartFpTop });
+    }
+})
+
+$(document).click(function (e) {
+    if ($('#cartFpContainer').is(':visible') && e.target.id != 'btnCartFp') {
+        $('#cartFpContainer').hide();
     }
 })
